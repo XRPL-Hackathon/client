faf478f88c11cbc9d703a2efbdc33fc0a592bd71
Eunjin Oh <ibsuny3214@naver.com>
2025-03-21 12:38:54 +0900

    Chore: page, router 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"), // 절대 경로로 src 디렉토리 설정
+    },
+  },
+});
