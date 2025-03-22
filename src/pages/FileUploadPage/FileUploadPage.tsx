73724354f5b911d3e82a70a4c6473f35df7c79c7
Eunjin Oh <ibsuny3214@naver.com>
2025-03-22 21:10:14 +0900

    Feat: 자료 업로드 클릭 시 데이터 가져오기 (#19)

--- src/pages/FileUploadPage/FileUploadPage.tsx
+++ src/pages/FileUploadPage/FileUploadPage.tsx
@@ -1,8 +1,24 @@
 import NavBar from "@/components/NavBar/NavBar";
 import * as S from "@/pages/FileUploadPage/FileUploadPage.style";
 import Upload from "./FileUpload";
+import { useState } from "react";
 
 const FileUploadPage = () => {
+  // 상태 선언
+  const [fileName, setFileName] = useState("");
+  const [fileCategory, setFileCategory] = useState("0");
+  const [filePrice, setFilePrice] = useState("");
+  const [fileDescription, setFileDescription] = useState("");
+
+  // form 제출 시 데이터 출력
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault(); // 기본 폼 제출 동작 방지
+    console.log("자료명:", fileName);
+    console.log("카테고리:", fileCategory);
+    console.log("가격:", filePrice);
+    console.log("자료 설명:", fileDescription);
+  };
+
   return (
     <>
       <NavBar />
@@ -11,23 +27,37 @@ const FileUploadPage = () => {
         <S.FileUploadContent>
           <Upload />
           <S.FileUploadDescription>
-            <form>
+            <form onSubmit={handleSubmit}>
               <ul className="ul">
                 <li className="li">
                   <label htmlFor="fileName" className="title">
                     자료명
                   </label>
-                  <input type="text" id="fileName" className="name_input" />
+                  <input
+                    type="text"
+                    id="fileName"
+                    className="name_input"
+                    value={fileName}
+                    onChange={(e) => setFileName(e.target.value)}
+                  />
                 </li>
                 <li className="li">
                   <label htmlFor="fileCategory" className="title">
                     카테고리
                   </label>
-                  <select id="fileCategory" className="select">
+                  <select
+                    id="fileCategory"
+                    className="select"
+                    value={fileCategory}
+                    onChange={(e) => setFileCategory(e.target.value)}
+                  >
                     <option value="0">카테고리 선택</option>
-                    <option value="1">카테고리1</option>
-                    <option value="2">카테고리2</option>
-                    <option value="3">카테고리3</option>
+                    <option value="1">시험 자료</option>
+                    <option value="2">자소서</option>
+                    <option value="3">이력서</option>
+                    <option value="4">레포트</option>
+                    <option value="5">악보</option>
+                    <option value="6">기타</option>
                   </select>
                 </li>
                 <li className="li">
@@ -39,6 +69,8 @@ const FileUploadPage = () => {
                       type="number"
                       id="filePrice"
                       className="price_input"
+                      value={filePrice}
+                      onChange={(e) => setFilePrice(e.target.value)}
                     />
                     &nbsp;XRP
                   </div>
@@ -47,11 +79,18 @@ const FileUploadPage = () => {
                   <label htmlFor="fileDes" className="title">
                     자료 설명
                   </label>
-                  <textarea id="fileDes" className="textarea" />
+                  <textarea
+                    id="fileDes"
+                    className="textarea"
+                    value={fileDescription}
+                    onChange={(e) => setFileDescription(e.target.value)}
+                  />
                 </li>
               </ul>
               <hr />
-              <button className="button"> 자료 업로드</button>
+              <button className="button" type="submit">
+                자료 업로드
+              </button>
             </form>
           </S.FileUploadDescription>
         </S.FileUploadContent>
